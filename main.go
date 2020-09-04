a57485d4784e967a298cc31c54aaca626ecc5101
sanderson.ad@googlemail.com <5hp31d26>
2020-09-04 17:31:01 +0100

    (1) changing paths to static data i.e. env, instruments etc. (2) adding port logic/default. (3) refactoring and tidying.

--- main.go
+++ main.go
@@ -8,29 +8,50 @@ import (
 	"sync"
 )
 
-var env = dict.LoadFromDir("/Users/andrewsanderson/Documents/dev/go/src/riskengine/data/env.json")
+// DEFAULTPORT : default port to listen on
+var DEFAULTPORT = ":8080"
 
 func main() {
 	// register the PID
 	log.Printf("process ID is %d", os.Getpid())
-	runLocal()
-	//runHTTP(":8080", "/price")
+	// get working directory
+	wd, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	// create an instance of a market data environment
+	env := dict.LoadFromDir(wd + "/data/env.json")
+	//runLocal(wd, env)
+	runHTTP("/price", env)
 }
 
-func runLocal() {
+func runLocal(wd string, env dict.Dict2) {
 	var wg sync.WaitGroup
 	// add some waits
 	wg.Add(4)
 	// price the products
-	go pricing.PriceFromDir(&wg, "/Users/andrewsanderson/Documents/dev/go/src/riskengine/data/bond_01.json", env)
-	go pricing.PriceFromDir(&wg, "/Users/andrewsanderson/Documents/dev/go/src/riskengine/data/bond_02.json", env)
-	go pricing.PriceFromDir(&wg, "/Users/andrewsanderson/Documents/dev/go/src/riskengine/data/bond_03.json", env)
-	go pricing.PriceFromDir(&wg, "/Users/andrewsanderson/Documents/dev/go/src/riskengine/data/europeancall_01.json", env)
+	go pricing.PriceFromDir(&wg, wd+"/data/bond_01.json", env)
+	go pricing.PriceFromDir(&wg, wd+"/data/bond_02.json", env)
+	go pricing.PriceFromDir(&wg, wd+"/data/bond_03.json", env)
+	go pricing.PriceFromDir(&wg, wd+"/data/europeancall_01.json", env)
 	// wait for the above routines to return
 	wg.Wait()
 }
 
-func runHTTP(port string, uri string) {
+func getPort() string {
+	// test for port
+	port := os.Getenv("PORT")
+	// default port
+	if port == "" {
+		port = DEFAULTPORT
+	}
+	log.Printf("listening on port %s", port)
+
+	return port
+}
+
+func runHTTP(uri string, env dict.Dict2) {
 	pricer := pricing.HTTPPricer{Env: env}
-	pricing.PriceFromHTTPRequests(pricer, port, uri)
+	// handle pricing requests
+	pricing.PriceFromHTTPRequests(pricer, getPort(), uri)
 }
