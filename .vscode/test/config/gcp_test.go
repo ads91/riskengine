9df80049d408b9597043ee0448dac697eb6786d0
sanderson.ad@googlemail.com <5hp31d26>
2020-10-09 22:46:44 +0100

    adding in test cases.

---
+++ .vscode/test/config/gcp_test.go
@@ -0,0 +1,4 @@
+package config
+
+// GCP_PROJECT_ID : the project ID to deploy to on GCP
+var GCP_PROJECT_ID = "ads-golang"
---
+++ .vscode/test/pricing/types_test.go
@@ -0,0 +1,30 @@
+package pricing
+
+import (
+	"riskengine/environment"
+	"riskengine/utils/dict"
+)
+
+// DailyTimeStep : a day expressed as a fraction of a year
+var DailyTimeStep = float64(1.0 / 365.0)
+
+// HTTPPricer : a wrapper for an HTTP handler func to accept a dict
+type HTTPPricer struct {
+	Env dict.Dict
+}
+
+// Bond : the representation of a bond
+type Bond struct {
+	Curve  *environment.Curve
+	Coupon float64
+}
+
+// EuropeanCall : the representation of a european call option
+type EuropeanCall struct {
+	s0  float64
+	K   float64
+	T   int     // years
+	R   float64 // decimal i.e. 0.05 = 5%
+	Vol float64
+	N   int // number of monte-carlo paths
+}
