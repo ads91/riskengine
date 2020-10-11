29c1a6b299e85f50f1b424687058d4aa5f943494
sanderson.ad@googlemail.com <5hp31d26>
2020-10-11 13:59:43 +0100

    fixing option price formula.

--- .vscode/test/config/gcp_test.go
+++
@@ -1,4 +0,0 @@
-package config
-
-// GCP_PROJECT_ID : the project ID to deploy to on GCP
-var GCP_PROJECT_ID = "ads-golang"
--- pricing/pricers.go
+++ pricing/pricers.go
@@ -3,6 +3,7 @@ package pricing
 import (
 	"math"
 	"math/rand"
+	"time"
 )
 
 // Price a bond
@@ -18,7 +19,8 @@ func (b Bond) Price() float64 {
 
 // Price a european call option
 func (ec EuropeanCall) Price() float64 {
-	// price
+	// seed time for monte-carlo simulation
+	rand.Seed(time.Now().UnixNano())
 	r := ec.R
 	t := ec.T
 	vol := ec.Vol
@@ -28,10 +30,15 @@ func (ec EuropeanCall) Price() float64 {
 	s1 := 0.0
 	for i := 0; i < n; i++ {
 		s2 := ec.s0
+		// run a price path
 		for j := 0; j < t*365; j++ {
 			s2 = s2 * (1.0 + r*dt + vol*math.Sqrt(dt)*rand.NormFloat64())
 		}
+		// calculate the payoff
 		s1 += math.Max(0.0, s2-k)
 	}
-	return float64(s1/float64((t*365)+1)) / math.Pow(1+r, float64(t))
+	// get the average of the simulations
+	s1 = s1 / float64(n)
+	// discount back to present day
+	return s1 / math.Pow(1+r, float64(t))
 }
